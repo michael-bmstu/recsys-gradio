c39464adecccb040917e79ea03685fe428a4187c
Mihail Solovev <solovei.0807@gmail.com>
2025-06-14 16:51:02 +0300

    fix: ошибка запуска пайплайна, выполнени пайплайн + хэширование данных

--- preprocess.py
+++ preprocess.py
@@ -1,2 +1,10 @@
 from app import utils
 utils.first_run()
+
+
+"""
+dvc stage add -n preprocess -d "data/anime.csv" -d "data/rating.csv" -p seed \
+-o "data/rating_clean.csv" -o "data/anime_clean.csv" -o "data/anime_genre.csv" -o "data/anime_train.csv" \
+-o "weights/kmeans.pkl" -o "weights/scaler.pkl" \
+"C:/Users/Михаил/Documents/venvs/gradio/Scripts/python.exe" preprocess.py
+"""
